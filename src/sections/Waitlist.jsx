ce339473e46ea0425e41aafcedf9526c5a1b3453
Chris Asek <chrisasek@gmail.com>
2024-01-31 23:24:01 +0100

    Update Waitlist.jsx

--- src/sections/Waitlist.jsx
+++ src/sections/Waitlist.jsx
@@ -5,8 +5,8 @@ const Waitlist = () => {
     return (
         <section className="">
             <div className="text-center">
-                <h1 className='text-5xl text-primary font-bold leading-tight mb-4'>Are you ready to grow your contact list and be discovered online?</h1>
-                <p className='text-lg leading-normal w-4/5 mx-auto'>Join our exclusive waitlist and be among the first to unlock opportunities for Your WhatsApp business! </p>
+                <h1 className='text-[32px] lg:text-5xl text-primary font-bold leading-tight mb-4'>Are you ready to grow your contact list and be discovered online?</h1>
+                <p className='text-[18px] lg:text-lg leading-normal w-4/5 mx-auto'>Join our exclusive waitlist and be among the first to unlock opportunities for Your WhatsApp business! </p>
             </div>
 
             <div className="flex justify-center items-center mt-10">

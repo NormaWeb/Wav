94a64e3c38ef269587f268aa3d4f7080df0c52b4
Chris Asek <chrisasek@gmail.com>
2024-01-31 23:41:31 +0100

    Update Unlock.jsx

--- src/sections/Unlock.jsx
+++ src/sections/Unlock.jsx
@@ -6,7 +6,7 @@ const Unlock = () => {
     return (
         <div className="">
             <div className='lg:max-w-[70%] lg:pt-10 '>
-                <h1 className='text-5xl 2xl:text-5xl lg:text-7xl/none  font-semibold mb-5  text-primary'>Unlock New Opportunities for Your WhatsApp Business!</h1>
+                <h1 className='text-[38px] lg:text-[3.2rem] leading-tight 2xl:text-5xl lg:text-7xl/none  font-semibold mb-5  text-primary'>Unlock New Opportunities for Your WhatsApp Business!</h1>
                 <p className='text-[23px]/normal md:text-3xl/snug mb-7  text-primary'>Welcome to <span className='font-semibold'>WAV</span>, the ultimate space for WhatsApp vendors to connect, grow their reach, and thrive in the digital marketplace.</p>
 
                 <Button url="#our-pricing" label="Join the waitlist" />

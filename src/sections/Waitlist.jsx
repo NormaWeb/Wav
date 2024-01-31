8311fa2e94d5c657c294c6d48f761a0a151eea91
Chris Asek <chrisasek@gmail.com>
2024-01-31 23:33:13 +0100

    Rearranged Footer and Callout Text

--- src/sections/Waitlist.jsx
+++ src/sections/Waitlist.jsx
@@ -6,7 +6,7 @@ const Waitlist = () => {
         <section className="">
             <div className="text-center">
                 <h1 className='text-[32px] lg:text-5xl text-primary font-bold leading-tight mb-4'>Are you ready to grow your contact list and be discovered online?</h1>
-                <p className='text-[18px] lg:text-lg leading-normal w-4/5 mx-auto'>Join our exclusive waitlist and be among the first to unlock opportunities for Your WhatsApp business! </p>
+                <p className='text-[18px] lg:text-[26px] leading- w-4/5 mx-auto'>Join our exclusive waitlist and be among the first to unlock opportunities for Your WhatsApp business! </p>
             </div>
 
             <div className="flex justify-center items-center mt-10">
